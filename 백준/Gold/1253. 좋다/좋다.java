82a0f9fa6c353f7e769b0a0e1db5daff2947633e
Jung Chaewon <aeea0520@gmail.com>
2024-10-15 23:03:51 +0900

    [Gold IV] Title: 좋다, Time: 188 ms, Memory: 16292 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -5,6 +5,13 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) throws IOException {
+        /*
+            2 pointer
+
+            - N개의 수들은 정수 (음의 정수, 0, 양의 정수 모두 포함)
+            - 어떤수 = 다른수 + 다른수 (자기 자신 제외)
+        */
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
@@ -19,31 +26,23 @@ class Main {
 
         Arrays.sort(num); // 정렬
         for(int i = 0; i < N; i++) { 
-            /*
-                2 pointer
-
-                - N개의 수들은 정수 (음의 정수, 0, 양의 정수 모두 포함)
-                - 어떤수 = 다른수 + 다른수 (자기 자신 제외)
-            */
-            
-            int start = (i == 0) ? 1 : 0;
-            int end = (i == N - 1) ? N - 2 : N - 1;
+            int start = (i == 0) ? 1 : 0; // 처음일 때 자기 자신 제외
+            int end = (i == N - 1) ? N - 2 : N - 1; // 끝일 때 자기 자신 제외
 
             while(start != end && (start < N && end > -1)) {
                 int sum = num[start] + num[end];
                 
                 if(sum == num[i]) {
-                    //System.out.println(num[start] + " + " + num[end] + " = " + sum);
                     count++;
                     break;
                 }
 
                 if(sum < num[i]) {
                     // start++;
-                    start = start + 1 == i ? start + 2 : start + 1;
+                    start = start + 1 == i ? start + 2 : start + 1; // 자기 자신일 때 하나 건너뜀
                 } else {
                     // end--;
-                    end = end - 1 == i ? end - 2 : end - 1;
+                    end = end - 1 == i ? end - 2 : end - 1; // 자기 자신일 때 하나 건너뜀
                 }
             }
         }  

21bfdd8e0e5cf999a36506889a455ca405b99e4f
Jung Chaewon <aeea0520@gmail.com>
2024-10-16 09:10:56 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1340 ms, Memory: 114612 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,44 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static final int MAX = 1000000;
+	public static void main(String[] args) throws IOException {
+		// case 1) 에라토스테네스의 체 -> 시간 초과 발생
+		Scanner sc = new Scanner(System.in);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		boolean[] isPrime = new boolean[MAX+1];
+		for(int i=2; i<=MAX; i++)
+			isPrime[i]=true;
+		
+		for(int i=2; i<=MAX; i++) {
+			for(int j=i*2; j<=MAX; j+=i) {
+				if(!isPrime[j]) continue;
+				isPrime[j]=false;
+			}			
+		}
+		
+		while(true) {
+			int x = sc.nextInt();
+			boolean ok = false;
+			
+			if(x==0)
+				break;
+			for(int i=2; i<=x/2; i++) {
+				if(isPrime[i] && isPrime[x-i]) {
+					bw.write(x + " = " + i + " + " + (x-i) + "\n");
+					ok=true;
+					break;
+				}
+			}
+			
+			if(!ok)
+				bw.write("Goldbach's conjecture is wrong.\n");
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}

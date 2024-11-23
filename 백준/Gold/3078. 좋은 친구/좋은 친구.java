d633c797abc26ba549c38ced686a05d25cdb6545
Jung Chaewon <aeea0520@gmail.com>
2024-11-23 17:52:27 +0900

    [Gold IV] Title: 좋은 친구, Time: 256 ms, Memory: 36972 KB -BaekjoonHub

--- 백준/Gold/3078. 좋은 친구/좋은 친구.java
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.java
@@ -4,26 +4,38 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int K = Integer.parseInt(input[1]); // 등수 차이 max
 
-        Queue<Integer>[] queues = new Queue[21];
-        for(int i = 0; i < 21; i++) {
-            queues[i] = new LinkedList<>();
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+        long count = 0; // 결과 저장
+        int[] nameLength = new int[N];
+        int[] goodFriend = new int[21]; // 이름 최대 길이는 20
+
+        for(int i = 0; i < N; i++) {
+            nameLength[i] = br.readLine().length();
         }
 
-        long count = 0;
-        for(int i = 0; i < N; i++) {
-            int length = br.readLine().length();
+        for(int i = 0; i < goodFriend.length; i++) { // 초기화
+            goodFriend[i] = 0;
+        }
 
-            while(!queues[length].isEmpty() && i - queues[length].peek() > K){
-                queues[length].poll();
+        // 슬라이딩 윈도우 사용하기 위해 첫 번째 작은 배열 먼저 계산
+        for(int i = 0; i < K + 1; i++) {
+            if(i < N) {
+                goodFriend[nameLength[i]]++;
             }
+        }
+        count += goodFriend[nameLength[0]] - 1; // 자기 자신 제외
 
-            count += queues[length].size();
-            queues[length].offer(i);
+        // 나머지 영역 계산
+        for(int i = 1; i < N; i++) {
+            goodFriend[nameLength[i - 1]]--; // 윈도우 영역이 아닌 값 제외
+            if(i + K < N) {
+                goodFriend[nameLength[i + K]]++; // 윈도우 영역인 값 삽입
+            }
+            count += goodFriend[nameLength[i]] - 1; // 자기 자신 제외
         }
 
         System.out.print(count);

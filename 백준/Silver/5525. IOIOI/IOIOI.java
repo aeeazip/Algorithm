6f3fbfdace3ac16e1c19922c665efa72db795ce0
Jung Chaewon <aeea0520@gmail.com>
2024-12-03 10:47:39 +0900

    [Silver I] Title: IOIOI, Time: 424 ms, Memory: 85500 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -7,26 +7,59 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine()); 
+        int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
 
-        int len = 3 + (N - 1) * 2;
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < len; i++) {
-            String s = i % 2 == 0 ? "I" : "O";
-            sb.append(s);
-        }
-
-        // 첫 번째 윈도우 계산
         int count = 0;
-        String input = br.readLine();
-        for(int i = 0; i <= input.length() - len; i++) {
-            String str = input.substring(i, i + len);
-            
-            if(str.contentEquals(sb))
+        int result = 0;
+
+        String[] str = br.readLine().split("");
+        for(int i = 0; i < str.length - 2; i++) {
+            if(str[i].equals("I") && str[i + 1].equals("O") && str[i + 2].equals("I")) {
                 count++;
+
+                if(count == N) {
+                    result++;
+                    count--;
+                }
+
+                i++;
+            } else {
+                count = 0;
+            }
         }
 
-        System.out.print(count);
+        System.out.print(result);
     }
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int N = Integer.parseInt(br.readLine());
+//        int M = Integer.parseInt(br.readLine());
+//
+//        int len = 3 + (N - 1) * 2;
+//        StringBuilder sb = new StringBuilder();
+//        for(int i = 0; i < len; i++) {
+//            String s = i % 2 == 0 ? "I" : "O";
+//            sb.append(s);
+//        }
+//
+//        // 첫 번째 윈도우 계산
+//        int count = 0;
+//        String input = br.readLine();
+//        for(int i = 0; i <= input.length() - len; i++) {
+//            if(input.charAt(i) != 'I')
+//                continue;
+//
+//            String str = input.substring(i, i + len);
+//
+//            if(str.contentEquals(sb)) {
+//                count++;
+//                i++;
+//            }
+//        }
+//
+//        System.out.print(count);
+//    }
 }

4c6f508ffbdbf07eee3390a47c271c88fe5ce0b2
Jung Chaewon <aeea0520@gmail.com>
2025-03-04 11:47:40 +0900

    [level 2] Title: 전화번호 목록, Time: 315.55 ms, Memory: 99.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

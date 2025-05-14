3f3b8ae292ba62a92d8af7d9868da1cf4b8ab0e7
Jung Chaewon <aeea0520@gmail.com>
2025-05-14 22:21:06 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,15 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] dp = new int[n+2];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 2];
+        dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2])%10007;
+
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
-        System.out.println(dp[n]%10007);
+
+        System.out.print(dp[n]);
     }
 }

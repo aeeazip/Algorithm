287a629dec5c131708abe443d94fb9063142dc96
Jung Chaewon <aeea0520@gmail.com>
2025-05-27 16:30:36 +0900

    [Silver I] Title: 문자열 교환, Time: 104 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.java
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -1,35 +1,35 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        int n = s.length();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
 
-        // 'a' 개수 세기
-        int aCnt = 0;
-        for (char c : s.toCharArray()) {
-            if (c == 'a') aCnt++;
+        // 1. a의 개수를 센다
+        long aCount = Arrays.stream(str.split(""))
+                .filter(s -> s.equals("a"))
+                .count();
+
+        // 2. 첫 윈도우에서 b의 개수를 센다.
+        int bCount = 0;
+        for(int i = 0; i < aCount; i++) {
+            if(str.charAt(i) == 'b') bCount++;
         }
 
-        // 문자열을 두 배로 확장
-        String extended = s + s;
+        int min = bCount;
+        String newStr = str + str; // 문자열이 원형 -> 슬라이딩 윈도우 적용을 위해 str + str
 
-        // 초기 윈도우 (길이 aCnt)에서 'b' 개수 세기
-        int bCnt = 0;
-        for (int i = 0; i < aCnt; i++) {
-            if (extended.charAt(i) == 'b') bCnt++;
-        }
-        int minSwap = bCnt; // 첫 구간에서 b 개수 기록
-
-        // 슬라이딩 윈도우로 최소 b 개수 찾기
-        for (int i = 1; i < n; i++) {
-            if (extended.charAt(i - 1) == 'b') bCnt--; // 왼쪽 끝 문자 제거
-            if (extended.charAt(i + aCnt - 1) == 'b') bCnt++; // 오른쪽 새 문자 추가
-
-            minSwap = Math.min(minSwap, bCnt);
+        for(int i = 1; i < str.length(); i++) {
+            if(newStr.charAt(i - 1) == 'b') bCount--;
+            if(newStr.charAt(i + (int) aCount -1) == 'b') bCount++;
+            
+            min = Math.min(min, bCount);
         }
 
-        System.out.print(minSwap);
+        System.out.print(min);
+
     }
 }

bc41606302f90d5146130a558e5418e2c7d557f8
Jung Chaewon <aeea0520@gmail.com>
2025-08-21 12:59:02 +0900

    [Gold IV] Title: 빙산, Time: 524 ms, Memory: 136592 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -2,16 +2,17 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
+// 12시 26분 start
 public class Main {
-    public static int N, M;
     public static int[][] map;
-    public static List<int[]> iceList;
-    public static boolean[][] visited;
+    public static int N, M;
     public static int[] dX = { -1, 1, 0, 0 };
-    public static int[] dY = { 0, 0, 1, -1 };
+    public static int[] dY = { 0, 0, -1, 1 };
+    public static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,78 +21,45 @@ public class Main {
         M = Integer.parseInt(input[1]);
 
         map = new int[N][M];
-        iceList = new ArrayList<>();
-
         for(int i = 0; i < N; i++) {
-            input = br.readLine().split(" ");
-            for(int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(input[j]);
-                if(map[i][j] != 0) iceList.add(new int[]{ i, j });
-            }
+            map[i] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
         }
 
-        int year = 1;
-
+        int year = 0;
         while(true) {
-            // 1. 다 녹았는지 체크
-            if(iceList.isEmpty()) {
-                System.out.print(0);
-                return;
-            }
-
-            // 2. 빙산 녹이기
-            melting();
-
-            // 3. dfs로 빙산 덩어리 세기
-            visited = new boolean[N][M];
-            int count = 0;
-
-            for(int i = 0; i < N; i++) {
-                for(int j = 0; j < M; j++) {
-                    if(!visited[i][j] && map[i][j] != 0) {
-                        count += dfs(i, j);
-                    }
-                }
-            }
-
-            if(count >= 2) {
-                System.out.print(year);
-                return;
+            int count = countIceberg();
+            if (count == 0 || count >= 2) {
+                if (count == 0) year = 0;
+                break;
             }
 
             year++;
+            melt();
         }
+
+        System.out.print(year);
     }
 
-    public static void melting() {
-        int[] melt = new int[iceList.size()];
+    // 빙산 덩어리 갯수 (다 녹았으면 0)
+    public static int countIceberg() {
+        int count = 0;
+        visited = new boolean[N][M];
 
-        for(int i = 0; i < iceList.size(); i++) {
-            int count = 0;
-
-            for(int j = 0; j < 4; j++) {
-                int newdX = iceList.get(i)[0] + dX[j];
-                int newdY = iceList.get(i)[1] + dY[j];
-
-                if(newdX >= 0 && newdX < N && newdY >= 0 && newdY < M && map[newdX][newdY] == 0)
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                if(!visited[i][j] && map[i][j] != 0) {
+                    dfs(i, j);
                     count++;
+                }
             }
-
-            melt[i] = count;
         }
 
-        List<int[]> newList = new ArrayList<>();
-        for(int i = 0; i < iceList.size(); i++) {
-            int x = iceList.get(i)[0];
-            int y = iceList.get(i)[1];
-            map[x][y] = Math.max(0, map[x][y] - melt[i]);
-            if(map[x][y] != 0) newList.add(new int[] { x, y });
-        }
-
-        iceList = newList;
+        return count;
     }
 
-    public static int dfs(int x, int y) {
+    public static void dfs(int x, int y) {
         visited[x][y] = true;
 
         for(int i = 0; i < 4; i++) {
@@ -103,8 +71,32 @@ public class Main {
                 dfs(newdX, newdY);
             }
         }
-
-        return 1;
     }
 
+    public static void melt() {
+        // 1. map 복사
+        int[][] mapClone = map.clone();
+        for(int i = 0; i < N; i++) {
+            mapClone[i] = map[i].clone();
+        }
+
+        // 2. 빙산 녹이기
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                if(mapClone[i][j] == 0) continue;
+
+                int now = mapClone[i][j];
+                for(int k = 0; k < 4; k++) {
+                    int newdX = i + dX[k];
+                    int newdY = j + dY[k];
+
+                    if(newdX < 0 || newdX >= N || newdY < 0 || newdY >= M) continue;
+                    if(mapClone[newdX][newdY] == 0) now--;
+                }
+
+                if(now < 0) now = 0;
+                map[i][j] = now;
+            }
+        }
+    }
 }
